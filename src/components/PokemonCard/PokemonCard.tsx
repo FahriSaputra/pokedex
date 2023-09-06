cbe1e3209996a0a3bcddf999e6a19c20dbc033c7
Fahri Saputra <fahrisaputra190@gmail.com>
2023-09-06 13:29:27 +0700

    feat: adjust key from order to id and add link to navigate

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import PokemonType from "@/components/PokemonType";
 import useGetPokemonDetail from "@/hooks/useGetPokemonDetail";
+import Link from "next/link";
 
 interface IPokemonCard {
   name: string;
@@ -16,31 +17,33 @@ const PokemonCard = (props: IPokemonCard) => {
   const image = data?.data?.sprites?.other?.["official-artwork"]?.front_default;
 
   return (
-    <div className="w-full border rounded-lg cursor-pointer hover:border-cyan-950">
-      <div className="flex bg-white rounded-lg flex-col">
-        <div className="flex justify-end flex-auto px-2 pt-2">
-          <p className="text-grey-medium text-xs font-normal">
-            #{data?.data?.order}
-          </p>
-        </div>
-
-        {!!image && (
-          <div className="relative w-full h-20 sm:h-32">
-            <Image src={image} alt={name} fill objectFit="contain" />
+    <Link href={`/${name}`}>
+      <div className="w-full border rounded-lg cursor-pointer hover:border-cyan-950">
+        <div className="flex bg-white rounded-lg flex-col">
+          <div className="flex justify-end flex-auto px-2 pt-2">
+            <p className="text-grey-medium text-xs font-normal">
+              #{data?.data?.id}
+            </p>
           </div>
-        )}
 
-        <div className="flex flex-col items-center bg-gray-background rounded-lg p-2">
-          <p className="pb-2">{name}</p>
+          {!!image && (
+            <div className="relative w-full h-20 sm:h-32">
+              <Image src={image} alt={name} fill objectFit="contain" />
+            </div>
+          )}
 
-          <div className="flex flex-start flex-wrap gap-2">
-            {types?.map((data) => (
-              <PokemonType key={data?.slot} type={data?.type?.name} />
-            ))}
+          <div className="flex flex-col items-center bg-gray-background rounded-lg p-2">
+            <p className="pb-2">{name}</p>
+
+            <div className="flex flex-start flex-wrap gap-2">
+              {types?.map((data) => (
+                <PokemonType key={data?.slot} type={data?.type?.name} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
