3fad87dc81338f218c803a77a0da460b0375d8c8
Fahri Saputra <fahrisaputra190@gmail.com>
2023-09-06 23:56:57 +0700

    feat: add empty state

--- src/app/favorite/page.tsx
+++ src/app/favorite/page.tsx
@@ -10,16 +10,23 @@ export default function Favorite() {
   return (
     <>
       <Header title="Favorite" bgColor="bg-pokemon-water" />
+      {favoritePokemon?.length === 0 && (
+        <div className="h-full text-center mt-24">
+          <p>Upss, Pokemon not Found</p>
+          <p>Add favorite pokemon first</p>
+        </div>
+      )}
 
       <div className="grid grid-cols-2 md:grid-cols-3 gap-2 px-2 mt-24">
-        {favoritePokemon?.map((species) => (
-          <PokemonCard
-            key={species?.name}
-            species={species}
-            isFavorite={isFavorite(species)}
-            onToggleFavorite={(el) => handleFavoritePokemon(el, species)}
-          />
-        ))}
+        {favoritePokemon &&
+          favoritePokemon?.map((species) => (
+            <PokemonCard
+              key={species?.name}
+              species={species}
+              isFavorite={isFavorite(species)}
+              onToggleFavorite={(el) => handleFavoritePokemon(el, species)}
+            />
+          ))}
       </div>
     </>
   );
