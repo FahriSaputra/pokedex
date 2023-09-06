6fcdd993e2cd9fc6c6b8ec710759fdf860457698
Fahri Saputra <fahrisaputra190@gmail.com>
2023-09-06 22:58:10 +0700

    chore: add primary color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
+        primary: "#DC0A2D",
         pokemon: {
           bug: "#A7B723",
           dark: "#75574C",
