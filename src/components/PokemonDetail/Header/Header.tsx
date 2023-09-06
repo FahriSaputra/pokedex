17ddb0e51ce39fb1f900e42b8251d5206a2c8863
Fahri Saputra <fahrisaputra190@gmail.com>
2023-09-06 19:32:05 +0700

    adjust dynamic color optional id

--- src/components/PokemonDetail/Header/Header.tsx
+++ src/components/PokemonDetail/Header/Header.tsx
@@ -5,13 +5,16 @@ import React from "react";
 interface IHeader {
   href?: string;
   title: string;
-  id: string | number;
+  id?: string | number;
+  bgColor?: string;
 }
 const Header = (props: IHeader) => {
-  const { href, title, id } = props;
+  const { href, title, id, bgColor } = props;
 
   return (
-    <div className="p-6 flex items-center gap-2 w-full absolute z-50">
+    <div
+      className={`p-6 flex items-center gap-2 w-full absolute z-50 ${bgColor}`}
+    >
       <Link href={href || "/"}>
         <Image
           className="cursor-pointer"
@@ -25,7 +28,7 @@ const Header = (props: IHeader) => {
       <h1 className="flex-1 text-2xl font-bold text-white capitalize text-ellipsis whitespace-nowrap">
         {title}
       </h1>
-      <p className="text-xs font-bold text-white">#{id}</p>
+      {id ? <p className="text-xs font-bold text-white">#{id}</p> : null}
     </div>
   );
 };
